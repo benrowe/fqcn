ecf66ab28d06ece9db3ed8028f39400c05575c75
Ben Rowe <ben.rowe.83@gmail.com>
2017-04-04 07:21:14 +1000

    Add PathBuilder component to handle building absolute paths from namespace

---
+++ src/PathBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Benrowe\Fqcn;
+
+use Benrowe\Fqcn\Value\Psr4Namespace;
+
+/**
+ * PathBuilder
+ *
+ * Build a path based on a namespace and the directory it's mapped to
+ *
+ * @package Benrowe\Fqcn
+ */
+class PathBuilder
+{
+    private $namespace;
+    private $path;
+
+    public function __construct(string $path, Psr4Namespace $namespace)
+    {
+        if (!is_dir($path)) {
+            throw new Exception('Invalid path');
+        }
+        $this->namespace = $namespace;
+        $this->path = $path;
+    }
+
+    public function resolve(Psr4Namespace $namespace): string
+    {
+        if (!$namespace->startsWith($this->namespace)) {
+            throw new Exception($namespace->getValue() . ' is not from the same base as ' . $this->namespace->getValue());
+        }
+        $relFqn = substr($namespace->getValue(), strlen($this->namespace->getValue()));
+        $relPath = $this->nsToPath($relFqn);
+
+        $absPath = realpath($this->path . DIRECTORY_SEPARATOR . $relPath);
+
+        return $absPath ?: '';
+    }
+
+    private function nsToPath(string $namespace): string
+    {
+        return trim(strtr($namespace, [
+            '\\' => DIRECTORY_SEPARATOR,
+            '//' => DIRECTORY_SEPARATOR
+        ]), '\\/');
+    }
+}
---
+++ tests/PathBuilderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Benrowe\Fqcn;
+
+use Benrowe\Fqcn\PathBuilder;
+use Benrowe\Fqcn\Value\Psr4Namespace;
+
+/**
+ * Class PathBuilder Test
+ *
+ * @package Benrowe\Fqcn\Tests\Unit
+ */
+class PathBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNamespacePath()
+    {
+        $namespace = new Psr4Namespace(__NAMESPACE__.'\\Test');
+        $builder = new PathBuilder(__DIR__, new Psr4Namespace(__NAMESPACE__));
+        $this->assertSame(realpath(__DIR__.DIRECTORY_SEPARATOR.'Test'), $builder->resolve($namespace));
+    }
+}
